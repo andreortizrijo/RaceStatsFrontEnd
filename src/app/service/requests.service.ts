a72228f4a4b833ad3e2e59117a1956d34f766ed9
André Rijo <andreortizrijo@gmail.com>
2021-05-27 16:46:25 +0100

    Model for login added / API Services added / Basic Dashboard & logout added

---
+++ src/app/service/requests.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestsService {
+
+  constructor(private http: HttpClient) { }
+
+  httpGET(api: string, header: any): Observable<HttpResponse<any>>{
+    return this.http.get(api,
+      {
+        headers: new HttpHeaders({
+          'token': header,
+        }),
+        observe: 'response',
+      }
+    );
+  };
+
+  httpPOST(api: string, body: any): Observable<HttpResponse<any>>{
+    return this.http.post(api, body, {
+      observe: 'response',
+    })
+  }
+}
